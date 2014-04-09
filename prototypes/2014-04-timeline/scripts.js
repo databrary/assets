9b5724788e701d92d0eecd6959ce9adfbd946fe5
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-04-09 16:56:49 -0400

    more styles for proto

--- prototypes/2014-04-timeline/scripts.js
+++ prototypes/2014-04-timeline/scripts.js
@@ -20,9 +20,9 @@ $(document).ready(function () {
 
 		$('.player-view').each(function () {
 			var $this = $(this),
-				$source = $this.find('.video-'+i);
+				$source = $this.find('.video-' + i);
 
-			if($source.length == 0)
+			if ($source.length == 0)
 				return;
 
 			var $main = $this.find('.player-view-main'),
@@ -32,5 +32,64 @@ $(document).ready(function () {
 			$source.after($target.detach());
 			$main.append($source.detach());
 		});
+	});
+
+	//
+
+	var $seekbar = $('.seekbar'),
+		$seekloaded = $seekbar.find('.seekbar-loaded'),
+		$seeksegment = $seekbar.find('.seekbar-segment'),
+		$seeknow = $seekbar.find('.seekbar-now');
+
+	$('.set-load').bind('click', function (e) {
+		var state = $(this).attr('data-toggle');
+
+		if (state.indexOf('all') > -1) {
+			$seekloaded.css({
+				left: 0,
+				right: '100%'
+			});
+			$seeksegment.css({
+				left: 0,
+				right: '100%'
+			});
+			$seeknow.css({
+				left: 0
+			});
+
+			$seekloaded.animate({
+				right: '0%'
+			}, 1000);
+		} else if (state.indexOf('segment') > -1) {
+			$seekloaded.css({
+				left: '50%',
+				right: '50%'
+			});
+			$seeksegment.css({
+				left: '50%',
+				right: '30%'
+			});
+			$seeknow.css({
+				left: '50%'
+			});
+
+			$seekloaded.animate({
+				right: '0%'
+			}, 1000);
+		}
+	});
+
+	$('.set-play').bind('click', function (e) {
+		var state = $(this).attr('data-toggle');
+
+		if (state.indexOf('all') > -1) {
+			$seeknow.animate({
+				left: '100%'
+			}, 15000, 'linear');
+		} else if (state.indexOf('segment') > -1) {
+			$seeknow.animate({
+				left: '70%'
+			}, 2000, 'linear');
+		}
 	});
 });
