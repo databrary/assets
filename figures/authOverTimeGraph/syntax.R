08844d5d49e990f6eccae909397ad338457d4d84
Gladys Chan <gladiolus.c@gmail.com>
2017-02-01 15:39:17 -0500

    auth graph updated to dec 2016

--- figures/authOverTimeGraph/syntax.R
+++ figures/authOverTimeGraph/syntax.R
@@ -16,20 +16,21 @@ ggplot(dataset, aes(x = V2, y = people)) +
   scale_x_date(breaks = seq(as.Date("2013-06-01"), as.Date("2016-06-13"), by = "6 month"), labels = date_format("%b %Y")) +
   scale_y_continuous(expand = c(0, 0)) +
   expand_limits(y = 0) +
+  expand_limits(y = 400) +
   theme(
     panel.background = element_blank(),
     panel.grid.minor = element_blank(),
     panel.grid.major.y = element_line(color="#cccccc",size=0.5),
-    axis.line.x = element_line(color="#333333",size=0.75),
-    axis.ticks.x = element_line(color="#333333",size=0.75),
+    axis.line.x = element_line(color="#333333",size=1),
+    axis.ticks.x = element_line(color="#333333",size=1),
     axis.ticks.y = element_blank(),
     axis.ticks.length = unit(.3, "cm"),
     axis.text.x = element_text(margin=margin(15,5,10,5,"pt")),
-    text = element_text(size=26)
+    text = element_text(size=28)
   ) +
   ylab("") +
   xlab("") +
-  annotate("text", x=as.Date('2015-03-01'), y=200, label = "Authorized Investigators", color = "#2FA38C", size=9) + 
-  annotate("text", x=as.Date('2016-03-01'), y=75, label = "Institutions", color="#EE9374", size=9)
+  annotate("text", x=as.Date('2015-02-15'), y=250, label = "Authorized Investigators", color = "#2FA38C", size=10) + 
+  annotate("text", x=as.Date('2016-04-01'), y=75, label = "Institutions", color="#EE9374", size=10)
   
-
+# 11in x 6in
